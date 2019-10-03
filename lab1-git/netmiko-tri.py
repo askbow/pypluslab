3b97002bad8c231403facdc4dcd08312e0e2c714
Denis Borchev <dborchev@gmail.com>
2019-10-02 22:51:43 -0700

    send show version to one cisco device

---
+++ lab1-git/netmiko-tri.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+from netmiko import ConnectHandler
+from getpass import getpass
+
+device = {
+"host"    : "cisco3.lasthop.io",
+"device_type" : "cisco_ios",
+"username"    : "pyclass",
+"password"    : getpass(),
+}
+
+
+nc = ConnectHandler( **device )
+
+print( nc.send_command('show version') )
